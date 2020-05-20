1b161c2f967ca06d4b1de6d5d4d734244c284999
Yuri <yuriy8888@yandex.ru>
2020-05-20 13:05:04 +0300

    order

---
+++ order.js
@@ -0,0 +1,9 @@
+function order(words) {
+  return words.split(' ')
+  .sort((a,b)=>a.match(/\d/)-b.match(/\d/))
+  .join(' ')
+}
+
+console.log(order("is2 Thi1s T4est 3a"));
+console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
+console.log(order(""));
