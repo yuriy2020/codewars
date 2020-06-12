c4aeeea11b11f71a9829a63625c045b5683e1453
Yuri <yuriy8888@yandex.ru>
2020-06-12 16:50:57 +0300

    strings

---
+++ fibonachi.js
@@ -0,0 +1,12 @@
+const fibo = (num) => {
+	let arr = [0];
+	let x=1;
+  for (let i = 0; i < num; i++) {
+		arr.push(i+j);
+		j=i;
+	}
+	console.log('>>>',arr);
+	
+};
+
+console.log(">>>", fibo(714));
